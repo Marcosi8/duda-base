5785252f8cd10c89e71d098e6eff0735ce03c567
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-01 18:59:11 -0300

    Update configuracao.js

--- configuracao.js
+++ configuracao.js
@@ -6,15 +6,15 @@ const chalk = require('chalk')
 // ============ [ CONFIGURAÇÃO ] ============ \\
 
 
-global.NomeDoDono = 'AlizinDev' //Preencha aqui como o primeiro campo Para o seu NOME
+global.NomeDoDono = 'marcoskz_' //Preencha aqui como o primeiro campo Para o seu NOME
 
-global.menu = 'https://telegra.ph/file/b2d75aaaed21ed7b91d49.jpg' //Preencha aqui como o primeiro campo Para a sua LOGO > FOTO DO MENU. ( OU DE ALGO QUE VC DEFINIR )
+global.menu = 'https://telegra.ph/Napole-02-01' //Preencha aqui como o primeiro campo Para a sua LOGO > FOTO DO MENU. ( OU DE ALGO QUE VC DEFINIR )
 
-global.NomeDobot = 'DUDA-BOT-BASE' //Preencha aqui como o primeiro campo Para o nome do seu BOT
+global.NomeDobot = 'Auto' //Preencha aqui como o primeiro campo Para o nome do seu BOT
 
 global.prefixo = '.' //Preencha aqui como o primeiro campo Para o seu prefixo ( COMANDO ANTES DO COMANDO )
 
-global.NumeroDoDonoA = ['5521989701174'] //Preencha aqui como o primeiro campo Para o seu NUMERO ( SEM O ' + ' VALEU )
+global.NumeroDoDonoA = ['558881647724'] //Preencha aqui como o primeiro campo Para o seu NUMERO ( SEM O ' + ' VALEU )
 
 
 ///// NÃO PRECISA MEXER AKI \\\\
@@ -27,12 +27,12 @@ global.banChats = false         /////
 
 
 
-global.author = 'AlizinDev' //Preencha aqui como o primeiro campo Para o seu NOME ( NOME DA FIGURINHA )
+global.author = 'autobahn' //Preencha aqui como o primeiro campo Para o seu NOME ( NOME DA FIGURINHA )
 
-global.packname = 'DUDA-BASE' // Preencha aqui como o primeiro campo visivel a marca d'agua dos stickers. ( NOME DA FIGURINHA )
+global.packname = 'auto' // Preencha aqui como o primeiro campo visivel a marca d'agua dos stickers. ( NOME DA FIGURINHA )
 
-global.packname2 = 'DUDA-MULTI' // Preencha aqui como o segundo campo visivel a marca d'agua dos stickers.
+global.packname2 = 'auto' // Preencha aqui como o segundo campo visivel a marca d'agua dos stickers.
 
 global.sessionName = 'QR-DA-DUDA-BOT' // N precisa alterar isso, este campo tem funcao de alterar o nome do qr-code.
 
-global.linkgrupss = 'https://chat.whatsapp.com/GcI1l0sflb2BMM199DmxOf' // Preencha com link do seu grupo ou deixe-o vazio.
+global.linkgrupss = '' // Preencha com link do seu grupo ou deixe-o vazio.
